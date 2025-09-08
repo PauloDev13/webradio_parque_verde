1e73923aeaa5dfc126587c127e7b34177389a8f3
Paulo Roberto De Morais <prmorais1302@gmail.com>
2025-09-08 16:34:03 -0300

    Changing the appearance of the wave form

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:just_audio/just_audio.dart';
-import 'package:siri_wave/siri_wave.dart';
+import 'package:waveform_visualizer/waveform_visualizer.dart';
 
 void main() {
+  WaveformVisualizer.initialize();
   runApp(const WebradioApp());
 }
 
@@ -32,24 +34,16 @@ class RadioPlayerPage extends StatefulWidget {
 class _RadioPlayerPageState extends State<RadioPlayerPage> {
   final AudioPlayer player = AudioPlayer();
   final String streamUrl = 'https://usa13.fastcast4u.com/proxy/parqueverde?mp=/1';
+  late WaveformController _waveController;
 
   String? _coverUrl;
   String? _lastSong;
 
-  late IOS9SiriWaveformController _waveformController;
 
   @override
   void initState() {
     super.initState();
-
-    _waveformController = IOS9SiriWaveformController(
-      color1: Color(0xFFAD394C),
-      color2: Color(0xFF30DC9B),
-      color3: Color(0xFF0F52A9),
-      amplitude: 1.0,
-      speed: 0.15,
-    );
-
+    _waveController = WaveformController();
     _startRadio();
   }
 
@@ -73,13 +67,17 @@ class _RadioPlayerPageState extends State<RadioPlayerPage> {
     if (player.playing) {
       await player.pause();
     } else {
-      // Reinicia stream para garantir áudio ao vivo
-      await player.setAudioSource(
-        AudioSource.uri(
-          Uri.parse(streamUrl),
-        ),
-      );
-      await player.play();
+      try {
+        // Reinicia stream para garantir áudio ao vivo
+        await player.setAudioSource(
+          AudioSource.uri(
+            Uri.parse(streamUrl),
+          ),
+        );
+        await player.play();
+      } catch (e) {
+        debugPrint("Erro ao iniciar rádio: $e");
+      }
     }
   }
 
@@ -97,7 +95,7 @@ class _RadioPlayerPageState extends State<RadioPlayerPage> {
     } catch (e) {
       debugPrint("Erro ao buscar capa: $e");
     }
-    return null;
+    return 'https://drive.google.com/file/d/1t_Q8u_ouwmjctpgSGzDMFnYhe8x8DKEb/view';
   }
 
   // Atualiza a capa de álbum sempre que a música mudar
@@ -116,25 +114,44 @@ class _RadioPlayerPageState extends State<RadioPlayerPage> {
   }
 
   String limparTitulo(String titulo) {
-    // Remove qualquer [conteudo] no final do título
+    // Remove qualquer [conteúdo] no final do título
     return titulo.replaceAll(RegExp(r'\s*\[[^\]]*\]$'), '').trim();
   }
 
-  Widget _formWave(bool playing) {
-    _waveformController.amplitude = playing ? 1.0 : 0.0;
-    _waveformController.speed = playing ? 0.15 : 0.0;
-    return SiriWaveform.ios9(
-      controller: _waveformController,
-      options: IOS9SiriWaveformOptions(
-        height: 100,
-        width: 250,
-      ),
-    );
+  Widget _waveFormControler(bool playing) {
+    if (playing) {
+      _waveController.start();
+      return WaveformWidget(
+        controller: _waveController,
+        height: 180,
+        width: 240,
+        style: WaveformStyle(
+          waveformStyle: WaveformDrawStyle.bars,
+          waveColor: Color(0xFF03ebff),
+          backgroundColor: Color(0x00ff5722),
+          barCount: 10,
+          barSpacing: 2.0,
+          strokeWidth: 2.0,
+          showGradient: true,
+          gradientBegin: Alignment.topCenter,
+          gradientEnd: Alignment.bottomCenter,
+          animationDuration: playing
+              ? Duration(milliseconds: 150)
+              : Duration.zero
+        ),
+      );
+    } else {
+      _waveController.stop();
+      return SizedBox(
+        height: 180,
+      );
+    }
   }
 
   @override
   void dispose() {
     player.dispose();
+    _waveController.dispose();
     super.dispose();
   }
 
@@ -184,93 +201,110 @@ class _RadioPlayerPageState extends State<RadioPlayerPage> {
                     _updateCover(rawTitle);
                   }
 
-                  return Column(
-                    children: [
-                      SizedBox(height: 10,),
-                      if (_coverUrl != null
-                          && _coverUrl!.isNotEmpty
-                          && artist.isNotEmpty)... [
-                        Image.network(
-                          _coverUrl!,
-                          height: 120,
-                          width: 120,
-                          fit: BoxFit.cover,
-                          errorBuilder: (context, error, stackTrace) =>
+                  return Padding(
+                    padding: const EdgeInsets.only(right: 20, left: 20),
+                    child: Column(
+                      children: [
+                        Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            StreamBuilder(
+                                stream: player.playingStream,
+                                initialData: player.playing,
+                                builder: (context, snapshot) {
+                                  final playing = snapshot.data ?? false;
+                                  return _waveFormControler(playing);
+                                }),
+                            if (_coverUrl != null
+                                && _coverUrl!.isNotEmpty
+                                && artist.isNotEmpty)... [
+                              Image.network(
+                                _coverUrl!,
+                                height: 140,
+                                width: 140,
+                                fit: BoxFit.cover,
+                                errorBuilder: (context, error, stackTrace) =>
+                                    Image.asset(
+                                      'assets/logo.png',
+                                      height: 140,
+                                      width: 140,
+                                      fit: BoxFit.cover,
+                                    ),
+                              ),
+                            ] else... [
                               Image.asset(
                                 'assets/logo.png',
-                                height: 150,
-                                width: 150,
+                                height: 140,
+                                width: 140,
                                 fit: BoxFit.cover,
                               ),
+                            ],
+                          ],
                         ),
-                      ] else... [
-                        Image.asset(
-                          'assets/logo.png',
-                          height: 120,
-                          width: 120,
-                          fit: BoxFit.cover,
+                        Text(
+                          artist.isNotEmpty
+                              ? artist
+                              :"Carregando artista...",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          song.isNotEmpty
+                              ? song
+                              :"Carregando música...",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontStyle: FontStyle.italic,
+                          ),
                         ),
                       ],
-
-                      // Exibe o wave form quando a música está tocando
-                      StreamBuilder(
-                          stream: player.playingStream,
-                          initialData: player.playing,
-                          builder: (context, snapshot) {
-                            // final playing = snapshot.data ?? false;
-                            return _formWave(true);
-                          }),
-                      Text(
-                        artist.isNotEmpty
-                            ? artist
-                            :"Carregando artista...",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                      Text(
-                        song.isNotEmpty
-                            ? song
-                            :"Carregando música...",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white,
-                            fontStyle: FontStyle.italic,
-                            // fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ],
+                    ),
                   );
                 }
               ),
               SizedBox(
-                height: 20,
+                height: 10,
               ),
               StreamBuilder(
                   stream: player.playingStream,
                   initialData: player.playing,
                   builder: (context, snapshot) {
                     final playing = snapshot.data ?? false;
+                    return FloatingActionButton(
+                      onPressed: _togglePlayPause,
+                      backgroundColor: Color(0x8003ebff),
+                      // child: playing ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+                      child: Icon(playing
+                          ? Icons.pause
+                          : Icons.play_arrow,
+                        size: 30,
+                      ),
+                    );
                     // return ElevatedButton.icon(
                     //     onPressed: _togglePlayPause,
                     //     icon: Icon(playing ? Icons.pause : Icons.play_arrow),
                     //     label: Text(playing ? 'Pausar' : 'Reproduzir'),
+                    //   style: ElevatedButton.styleFrom(
+                    //     backgroundColor: Color(0xFF03ebff),
+                    //   ),
                     // );
-                    return IconButton(
-                        onPressed: _togglePlayPause,
-                        icon: Icon(
-                          playing
-                              ? Icons.pause
-                              : Icons.play_arrow,
-                          size: 50,
-                          color: Color((0xFF03ebff),
-                          ),
-                        ),
-                    );
+                    // return IconButton(
+                    //     onPressed: _togglePlayPause,
+                    //     icon: Icon(
+                    //       playing
+                    //           ? Icons.pause
+                    //           : Icons.play_arrow,
+                    //       size: 50,
+                    //       color: Color((0xFF03ebff),
+                    //       ),
+                    //     ),
+                    // );
                   }
               ),
             ],
